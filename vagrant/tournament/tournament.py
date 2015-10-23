0378ba0b337ed7544a27b56d3a0c898a0bb15240
Hao Wu <tohaowu@gmail.com>
2015-10-23 15:13:44 -0700

    backup to old code

--- vagrant/tournament/tournament.py
+++ vagrant/tournament/tournament.py
@@ -85,7 +85,8 @@ def reportMatch(winner, loser):
       loser:  the id number of the player who lost
     """
     db, cursor = connect()
-    cursor.execute("INSERT INTO Matches (player, opponent) VALUES (%s, %s)", (winner, loser))
+    cursor.execute("INSERT INTO Matches (player, opponent, result) VALUES (%s, %s, 1)", (winner, loser))
+    cursor.execute("INSERT INTO Matches (player, opponent, result) VALUES (%s, %s, 0)", (loser, winner))
     db.commit()
     db.close()
  
